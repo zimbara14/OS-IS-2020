fafd9a8ae4f172c030f72590c57d607a65d69381
zimbara14 <winterking000@gmail.com>
2020-09-22 07:42:41 +0300

    text_log.sh

--- text_log.sh
+++ text_log.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
-find /var/log/anaconda -type f -iname "X.log" > newfile
-#sed 's/warn/\033[33mWarning:\e[0m/g' | sed 's/info/\033[34mInformation:\e[0m/g' <<< $file
-echo "ye"
+ffile="/var/log/anaconda/X.log"
+
+if [[ ! -f $ffile ]]; then
+	echo "Error, tried to open non-existing file. Try again!"
+	exit 1
+fi
+
+#	cat $ffile | grep "(WW)" | grep -v "warning " | sed 's/WW/\033[33mWarning:\e[0m/g'
+#	cat $ffile | sed 's/info/\033[34mInformation:\e[0m/g'
+
+echo "This was log"
+warning=$(cat $ffile | sed 's/WW/\e[33mWarning:\e[0m/g')
+info=$(cat $ffile | sed 's/II/\e[34mInformation:\e[0m/g')
+
+echo -e "${warning}"
+echo -e "${info}"
+
+

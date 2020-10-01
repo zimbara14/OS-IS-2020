17de760a5dcde80d99049d628e7b02a0aa912d43
zimbara14 <winterking000@gmail.com>
2020-10-01 13:43:00 +0300

    text_interactive.sh

--- text_interactive.sh
+++ text_interactive.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-echo "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -"
-echo $'\033[96m     *     INTERACTIVE MENU     *             \e[0m'
-echo $'You can:\na - use calculator (command is: \033[31mcalc\e[0m)\nb - use reverse (command is: \033[32mreverse\e[0m)\nc - find the length of a string (command is: \033[33mstrlen\e[0m)\nd - log\ne - exit with a code (command is: \033[35mexit\e[0m)\nf - see manual for everything (command is: \033[36mhelp\e[0m)\ng - use function search (command is: \033[34msearch\e[0m)\nTo use the command you like, you just need to input either the letter of the command or the specific coloured identifier. Try!'
+echo -e "\033[43m- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -\e[0m"
+echo $'\033[96m		  		   *     INTERACTIVE MENU     *\e[0m'
+echo $'You can:\na - use calculator (command is: \033[31mcalc\e[0m)\nb - use reverse (command is: \033[32mreverse\e[0m)\nc - find the length of a string (command is: \033[33mstrlen\e[0m)\nd - warnings and information (command is: \033[92mlog\e[0m)\ne - exit with a code (command is: \033[35mexit\e[0m)\nf - see manual for everything (command is: \033[36mhelp\e[0m)\ng - use function search (command is: \033[34msearch\e[0m)\nTo use the command you like, you just need to input either the letter of the command or the specific coloured identifier. Try!'
 read inp
 case "$inp" in
-	calc | a)
+	calc|a)
 	echo "Enter sum/sub/mul/div and two ints in one line!"
 	read var n1 n2
 	var2 = &(./text_calc.sh " " "$var" "$n1" "$n2")
@@ -12,14 +12,14 @@ case "$inp" in
 	. ./text_interactive.sh
 	;;
 
-	reverse | b)
+	reverse|b)
 	echo "Enter the two name files"
 	read var3 var4
 	echo "Done! Check out the $var4 file!"
 	. ./text_interactive.sh
 	;;
 
-	strlen | c)
+	strlen|c)
 	echo "Enter a string:"
 	read str
 	var6 = &(./text_string.sh " " "$str")
@@ -28,44 +28,53 @@ case "$inp" in
 	. ./text_interactive.sh
 	;;
 	
-	log | d)
+	log|d)
 	var7=&(./text_log.sh)
 	. ./text_interactive.sh
 	;;
 	
-	exit | e)
+	exit|e)
 	echo "Would you like to input exit code? If not, just hit the enter button"
 	read varr
-	
-	if [[ -z $varr ]]; then
-		echo "Just exiting..."
-		exit 0
-	fi
+	. ./text_exit.sh " " "$varr"
+	#varia = $?
+	#exit $varia
+	#if [[ -z $varr ]]; then
+	#	echo "Just exiting..."
+	#	exit 0
+	#	fi
 
-	if [[ ! $varr =~ ^[\+|-]?[0-9]+$ ]]; then
-		echo "Exiting..."
-		exit 1
-	else
-		echo "Exiting with a code... $varr"
-		exit $varr
-	fi
+	#if [[ ! $varr =~ ^[\+|-]?[0-9]+$ ]]; then
+	#	echo "Exiting..."
+	#	exit 1
+	#if [[ $var -ge 0 && $var -le 255 ]]; then
+	#	echo "Exiting with a code... $varr"
+	#	exit "$varr"
+	#else 
+	#	echo "Exiting..."
+	#	exit1
+	#fi
 	;;
 
-	help | f)
+	help|f)
 	echo "This is a manual."
 	echo "$(<help.txt)"
 	echo " "
 	./text_interactive.sh
 	;;
 
-	search | g)
+	search|g)
 	echo "Enter directory and a regular expression"
 	read diir reeg
-	var9=&(./text_search.sh " "$diir" "$reeg")
-	;;
-	*)
-	echo "Invalid option. Try again."
+	var9=&(./text_search.sh " " "$diir" "$reeg")
 	. ./text_interactive.sh
 	;;
+
+	*)
+	echo $'\033[31mInvalid option. Try again.\e[0m'
+	. ./text_interactive.sh
+	;;
+
 esac
 
+exit 0

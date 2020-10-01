60042401dcecd63e1d2571255ec3473edc048a8a
zimbara14 <winterking000@gmail.com>
2020-10-01 13:41:16 +0300

    text_reverse.sh

--- text_reverse.sh
+++ text_reverse.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
-text1=$2
-text2=$3
-tac $text1 > $3
+
+#text1=$2
+#text2=$3
+
+if ! [[ -e $2 ]]; then
+	echo "File does not exist! Terminating..."
+	exit 1
+fi
+
+if ! [[ -e $3 ]]; then
+	echo "Self-reverse activated."
+	tac $2 | rev > $2
+	exit 0
+else
+	tac $2 > $3 | 2>/dev/null
+	exit 0
+fi
+
+
+
+
 
 exit 0

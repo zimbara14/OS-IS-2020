554901d1e8b7ba3228b1addd405f52fb33c352dc
zimbara14 <winterking000@gmail.com>
2020-10-02 14:08:06 +0300

    text_exit.sh

--- text_exit.sh
+++ text_exit.sh
@@ -1,12 +1,14 @@
 
 #!/bin/bash
 
-if ! [[ $2 -ge 0  && $2 -le 255 ]]; then
-	echo "Not in range."
+if [[ ! "$2" =~ ^[+-]?[0-9]+$ ]]; then
+	echo "NaN! Exiting with code 1..."
 	exit 1
 fi
-if [[ ! "$2" =~ ^[+-]?[0-9]+$ ]]; then
-	exit 0
+
+if ! [[ $2 -ge 0  && $2 -le 255 ]]; then
+	echo "Not in range. Exiting with code 1..."
+	exit 1
 fi
 
 if [ -z "$2" ]

d70d36de95db4916aed8946be41084136c4e4fe7
zimbara14 <winterking000@gmail.com>
2020-10-02 13:37:29 +0300

    text_exit.sh

--- text_exit.sh
+++ text_exit.sh
@@ -2,10 +2,12 @@
 #!/bin/bash
 
 if ! [[ $2 -ge 0  && $2 -le 255 ]]; then
+	echo "Not in range."
 	exit 1
 fi
 if [[ ! "$2" =~ ^[+-]?[0-9]+$ ]]; then
-	exit 0
+	echo "Not a number. Exiting with code 1."
+	exit 1
 fi
 
 if [ -z "$2" ]
